963df12844944b92230a6c927a05904b0c6fbb33
Snail Mail <32091869+nihilsen@users.noreply.github.com>
2023-02-10 03:58:25 +0000

    test: Set up tests for Github workflows

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace Nihilsen\Cipher\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Nihilsen\Cipher\CipherServiceProvider;
 
@@ -11,10 +10,6 @@ class TestCase extends Orchestra
     protected function setUp(): void
     {
         parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Nihilsen\\Cipher\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
     }
 
     protected function getPackageProviders($app)
