e0dd1072448d09519965e02bbb10f6e347305e3a
Snail Mail <32091869+nihilsen@users.noreply.github.com>
2023-02-25 12:48:37 +0000

    test: Improve database migrations for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Nihilsen\Cipher\Tests;
 
 use Nihilsen\Cipher\CipherServiceProvider;
+use function Orchestra\Testbench\artisan;
 use Orchestra\Testbench\Dusk\TestCase as Base;
 
 class TestCase extends Base
@@ -15,6 +16,12 @@ class TestCase extends Base
     protected function defineDatabaseMigrations()
     {
         $this->loadLaravelMigrations();
+
+        artisan($this, 'migrate', ['--database' => 'sqlite']);
+
+        $this->beforeApplicationDestroyed(
+            fn () => artisan($this, 'migrate:rollback', ['--database' => 'sqlite'])
+        );
     }
 
     public function getEnvironmentSetUp($app)
